12732aae31bbe1498821985c3f19fbbb9827bd76
Tamás Komoróczy <szolarisz@gmail.com>
2021-10-21 19:03:58 +0200

    githubon módosítva

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ const http= require('http');
 const fs = require('fs');
 const url = require('url');
 
-console.log("Késtzül a routerünk");
+console.log("Készül a routerünk");
+console.log("Mi a manó?");
